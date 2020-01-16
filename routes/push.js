356b7bfb273f4a5fe26af2b7bcc5e3d05bb18f41
Space School <space.school.kg@gmail.com>
2020-01-16 19:38:36 +0600

    add webpush

--- routes/push.js
+++ routes/push.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { getAdminId } = require('../module/user');
 const { sendWebPush } = require('../module/webPush');
+const UserAzyk = require('../models/userAzyk');
 
-router.get('/admin', (req, res) => {
-    console.log(getAdminId())
-    if(getAdminId()){
-        sendWebPush('AZYK.STORE', 'Не забудьте сделать свой заказ', getAdminId())
+router.get('/admin', async (req, res) => {
+    let user = await UserAzyk.findOne({role: 'admin'})
+    if(user){
+        sendWebPush('AZYK.STORE', 'Не забудьте сделать свой заказ', user._id)
         res.json('Push triggered');
     }
     else {
