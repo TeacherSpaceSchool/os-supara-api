096020393c10a95f346f5e378687aea36b62fe9c
Space School <space.school.kg@gmail.com>
2020-04-01 02:25:15 +0600

    check trash

--- module/outXMLShoroAzyk.js
+++ module/outXMLShoroAzyk.js
@@ -45,8 +45,7 @@ module.exports.setOutXMLReturnedShoroAzyk = async(returned) => {
                     .findOne({ecspeditor: district.ecspeditor})
                 if (guidAgent && guidEcspeditor) {
                     let date = new Date()
-                    console.log(date)
-                    if(date.getHours()<18)
+                    if(date.getHours()<22)
                         date.setDate(date.getDate() + 1)
                     let newOutXMLReturnedShoroAzyk = new OutXMLReturnedShoroAzyk({
                         data: [],
