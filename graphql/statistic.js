6fdb370822a792a8db7f3f6fa7ed441031f19054
Space School <space.school.kg@gmail.com>
2020-04-06 02:01:37 +0600

    check apk

--- graphql/statistic.js
+++ graphql/statistic.js
@@ -77,11 +77,11 @@ const resolvers = {
                     .lean()
                 for (let i = 0; i < data.length; i++) {
                     if (type === 'повторяющиеся guid') {
-                        problem = (
-                            data.filter(element => {
-                                return element.guid === data[i].guid
-                            })
-                        ).length > 1
+                        problem = 0
+                        for (let i1 = 0; i1 < data.length; i1++) {
+                            if(element.guid === data[i].guid)
+                                problem += 1
+                        }
                     }
                     else if (type === 'повторящиеся клиенты') {
                         problem = 0
@@ -92,33 +92,35 @@ const resolvers = {
                         problem = problem>1
                     }
                     else {
-                        if (data[i].client.address && data[i].client.address[0] && data[i].client.address[0][0] && data[i].client.address[0][2]) {
+                        if (data[i].client.address && data[i].client.address[0]/* && data[i].client.address[0][0]*/ && data[i].client.address[0][2]) {
                             problem = 0
                             //let address = data[i].client.address[0][0].toLowerCase()
                             let market = data[i].client.address[0][2].toLowerCase()
                             /*while (address.includes(' '))
-                                address = address.replace(' ', '')
-                            while (address.includes('-'))
-                                address = address.replace('-', '')*/
+                                                           address = address.replace(' ', '')
+                                                       while (address.includes('-'))
+                                                           address = address.replace('-', '')*/
                             while (market.includes(' '))
                                 market = market.replace(' ', '');
                             while (market.includes('-'))
                                 market = market.replace('-', '');
                             for(let i1 = 0; i1 < data.length; i1++) {
-                                //let address1 = element.client.address[0][0].toLowerCase()
-                                let market1 = data[i1].client.address[0][2].toLowerCase()
-                                /*while (address1.includes(' '))
-                                       address1 = address1.replace(' ', '')
-                                   while (address1.includes('-'))
-                                        address1 = address1.replace('-', '')*/
-                                while (market1.includes(' '))
-                                    market1 = market1.replace(' ', '')
-                                while (market1.includes('-'))
-                                    market1 = market1.replace('-', '')
-                                if(/*address1 === address ||*/market1 === market)
-                                    problem += 1
-                                problem = problem>1
+                                if (data[i1].client.address && data[i1].client.address[0]/* && data[i1].client.address[0][0]*/ && data[i1].client.address[0][2]) {
+                                    //let address1 = element.client.address[0][0].toLowerCase()
+                                    let market1 = data[i1].client.address[0][2].toLowerCase()
+                                    /*while (address1.includes(' '))
+                                                                               address1 = address1.replace(' ', '')
+                                                                           while (address1.includes('-'))
+                                                                                address1 = address1.replace('-', '')*/
+                                    while (market1.includes(' '))
+                                        market1 = market1.replace(' ', '')
+                                    while (market1.includes('-'))
+                                        market1 = market1.replace('-', '')
+                                    if (/*address1 === address ||*/market1 === market)
+                                        problem += 1
+                                }
                             }
+                            problem = problem>1
                         }
                     }
                     if (problem) {
