db7940a7cda28c4ba7bbed1ca280e230fbde5dca
Space School <space.school.kg@gmail.com>
2020-04-17 21:44:31 +0600

    check adss statistic

--- thread/outXMLShoroAzyk.js
+++ thread/outXMLShoroAzyk.js
@@ -4,7 +4,7 @@ const { reductionOutAdsXMLShoroAzyk } = require('../module/outXMLShoroAzyk');
 const cron = require('node-cron');
 connectDB.connect()
 if(!isMainThread) {
-    cron.schedule('35 1 * * *', async() => {
+    cron.schedule('1 3 * * *', async() => {
         await reductionOutAdsXMLShoroAzyk()
     });
 }
